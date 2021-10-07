565732a9fd8542611d88075ed3558fe43e5a566d
Muhammad Shaheryar Haider <shaheryarjerry@gmail.com>
2021-10-07 07:41:17 +0500

    Working On Charts

--- src/IncomeAndExpense.js
+++ src/IncomeAndExpense.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import './IncomeAndExpense.css'
-import { IconButton } from '@material-ui/core'
+import incomeandexpense from './IncomeAndExpense.json'
 const IncomeAndExpense = () =>
 {
+    const Chart = ({ children, width, height }) => (
+        <svg viewBox={`0 0 ${width} ${height}`}
+            height={height}
+            width={width}>
+            {children}
+        </svg>
+    )
+    const Bar = ({fill='#000', x,y,height,width}) => (
+        <rect fill ={fill} x={ x} y={y} height ={height} width ={width}  />
+    )
+
+    const greatestValue = values => values.reduce((acc,cur) =>(cur>acc? cur:acc), -Infinity)
+    const BarChart = ({incomeandexpense}) =>
+    {
+        const barWidth = 20
+        const barMargin = 5
+        const width = incomeandexpense.length * (barWidth + barMargin)
+        const height =greatestValue(incomeandexpense.map(datum => datum.quantity))
+        return (
+            <Chart height ={height} width = {width}>
+                {incomeandexpense.map((datum, index)=>(
+                <Bar
+                        key={datum.name}
+                        fill ='teal'
+                    x={index * (barWidth + barMargin)}
+                    y={height -datum.quantity}
+                    width={barWidth}
+                    height ={datum.quantity}/>
+                ))}
+            </Chart>
+        )
+    }
     return (
         <div className = 'feeContainer'> 
             <h4>Fees Collection and Expenses</h4>
             <div className = 'feeDes'>
                 <div className='feeDesLeft'>
-                
                     <div className='feeDesLeftElement'>
                         <strong>$ 10,000</strong>
                         <p>Collections</p>
@@ -21,7 +52,10 @@ const IncomeAndExpense = () =>
                         <strong>$ 4,000</strong>
                         <p>Expenses</p>
                     </div>
-
+                 
+                </div>
+                <div className ='chart'>
+                <BarChart incomeandexpense={ incomeandexpense}/>   
                 </div>
             </div>
         </div>
